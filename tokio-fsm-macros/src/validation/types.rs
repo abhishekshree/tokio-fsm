e5f4a4c44b3df224b2478a9892b6588916681a1b
Abhishek Shree <abhishek.shree@outlook.com>
2026-05-23 22:50:50 +0530

    refactor(macros): add FSM IR domain types

--- tokio-fsm-macros/src/validation/types.rs
+++ tokio-fsm-macros/src/validation/types.rs
@@ -1,5 +1,92 @@
 use syn::{Ident, Type};
 
+/// Name of the generated FSM type.
+#[allow(dead_code)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct FsmName(pub Ident);
+
+impl From<Ident> for FsmName {
+    fn from(name: Ident) -> Self {
+        Self(name)
+    }
+}
+
+impl From<&Ident> for FsmName {
+    fn from(name: &Ident) -> Self {
+        Self(name.clone())
+    }
+}
+
+impl AsRef<Ident> for FsmName {
+    fn as_ref(&self) -> &Ident {
+        &self.0
+    }
+}
+
+/// Name of an FSM state.
+#[allow(dead_code)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct StateName(pub Ident);
+
+impl From<Ident> for StateName {
+    fn from(name: Ident) -> Self {
+        Self(name)
+    }
+}
+
+impl From<&Ident> for StateName {
+    fn from(name: &Ident) -> Self {
+        Self(name.clone())
+    }
+}
+
+impl AsRef<Ident> for StateName {
+    fn as_ref(&self) -> &Ident {
+        &self.0
+    }
+}
+
+/// Name of an FSM event.
+#[allow(dead_code)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct EventName(pub Ident);
+
+impl From<Ident> for EventName {
+    fn from(name: Ident) -> Self {
+        Self(name)
+    }
+}
+
+impl From<&Ident> for EventName {
+    fn from(name: &Ident) -> Self {
+        Self(name.clone())
+    }
+}
+
+impl AsRef<Ident> for EventName {
+    fn as_ref(&self) -> &Ident {
+        &self.0
+    }
+}
+
+/// Macro configuration derived from FSM attributes.
+#[allow(dead_code)]
+#[derive(Debug, Clone)]
+pub struct FsmConfig {
+    pub initial: StateName,
+    pub channel_size: usize,
+    pub tracing: bool,
+    pub serde: bool,
+}
+
+/// Required associated types declared on the FSM impl.
+#[allow(dead_code)]
+#[derive(Debug, Clone)]
+pub struct AssociatedTypes {
+    pub context: Type,
+    pub error: Type,
+}
+
 /// Represents a discovered state in the FSM.
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct State {
